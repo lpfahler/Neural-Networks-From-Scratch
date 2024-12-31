a77ca4cde0aa535cb57c4aacde12e1a4c07fab1f
Lori Pfahler <lpfahler@comcast.net>
2024-12-31 09:03:06 -0500

    Chapter 2 Code

---
+++ C2 A Single Neuron.py
@@ -0,0 +1,28 @@
+# Chapter 2 A Single Neuron
+# Lori Pfahler
+# December 2024
+
+# example of one neuron with three inputs 
+# our variables - most likely scaled/normed
+inputs = [1, 2, 3]
+# weights for the three inputs - chosen randomly to start usually
+weights = [0.2, 0.8, -0.5]
+# one bias value per neuron
+bias = 2
+# output of the neuron
+output = (inputs[0]*weights[0] + 
+          inputs[1]*weights[1] +
+          inputs[2]*weights[2] + 
+          bias)
+
+print(f'output = {output})
+
+# four inputs to the neuron
+weights = [0.2, 0.8, -0.5, 2.5]
+output = (inputs[0]*weights[0] + 
+          inputs[1]*weights[1] +
+          inputs[2]*weights[2] + 
+          inputs[3]*weights[3]
+          bias)
+
+print
