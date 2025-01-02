fddac5a21c596e15e3cb04b4095d43a33f8b47c3
Lori Pfahler <lpfahler@comcast.net>
2025-01-02 14:02:32 -0500

    Starting to work with NumPy

---
+++ C2 Dot Product.py
@@ -0,0 +1,10 @@
+# Chapter 2 A Layer of Neurons pp 38
+# dot product of vectors
+# Lori Pfahler
+# January 2025
+
+a = [1, 2, 3]
+b = [2, 3, 4]
+
+dot_product = a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+print(dot_product)
---
+++ C2 Single Neuron with NumPy.py
@@ -0,0 +1,17 @@
+# Chapter 2 Single Neuron with NumPy
+# Lori Pfahler
+# January 2025
+
+import numpy as np
+
+# example of one neuron with four inputs 
+# our variables - most likely scaled/normed
+inputs = [1, 2, 3, 2.5]
+# weights for the four inputs - chosen randomly to start usually
+weights = [0.2, 0.8, -0.5, 1]
+# one bias value per neuron
+bias = 2
+
+outputs = np.dot(weights, inputs) + bias
+
+print(outputs)
