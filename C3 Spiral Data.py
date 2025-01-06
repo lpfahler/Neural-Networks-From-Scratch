d344aaadb600982d5998507d693c936c92b135d6
Lori Pfahler <lpfahler@comcast.net>
2025-01-06 14:11:53 -0500

    Chapter 3 update spiral data

--- C3 Spiral Data.py
+++ C3 Spiral Data.py
@@ -6,12 +6,15 @@
 # must use Python 3.12.2('Base':conda) - somehow I installed nnfs module in 
 # the anaconda version?  Need to spend more time with VS Code and figuring out
 # how to direct installation of modules to correct version of python
+import nnfs.datasets
 import numpy as np
 import nnfs
-nnfs.init()
 # had to import this separately to use
 from nnfs.datasets import spiral_data
+# plotting module
 import matplotlib.pyplot as plt
+
+nnfs.init()
 
 # create training data
 X, y = spiral_data(samples = 100, classes = 3)
@@ -24,8 +27,7 @@ plt.show()
 print(X)
 print(y)
 
-
-# plot of two x variables with classification - variable y 
+# plot of two x variables with classification - variable y (0, 1, or 2)
 # shown in color
 plt.scatter(X[:, 0], X[:, 1], c = y, cmap = 'brg')
 plt.show()
